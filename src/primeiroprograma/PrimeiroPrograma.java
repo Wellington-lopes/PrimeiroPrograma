927a88b2b25cbbf1d76fbcc9a81d9e0e4cf819e7
Wellington Lopes <wellington.slopes1991@gmail.com>
2023-03-29 18:56:53 -0300

    Aprendendo JAVA
    Olá, Mundo!

---
+++ src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primeiroprograma;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+    
+}
